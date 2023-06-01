1c2308a3f34ea713033b7a9cbcbe7d4838772f5f
Jamie Smith <me@jsmith.dev>
2023-06-01 12:17:31 -0400

    add generate UUID

--- src/modules/utils/strings.js
+++ src/modules/utils/strings.js
@@ -15,3 +15,16 @@ export function capitalize(s) {
 export function camelCase(string) {
 	return string.replace(/-([a-z])/gi, (all, letter) => letter.toUpperCase());
 }
+
+/**
+ * Function to generate and return a new UUID
+ * @returns {String}
+ * @example
+ * const uuid = generateUUID();
+ * console.log(uuid);
+ * // => '110ec58a-a0f2-4ac4-8393-c866d813b8d1'
+ * */
+export function generateUUID() {
+	return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
+}
